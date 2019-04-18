17a3686d241e86eefaa6f82d45dde09376e9d295
Ryan White <WHITER23@my.erau.edu>
2019-04-18 10:28:52 -0400

    test
    created two warriors that duel to the death

---
+++ WarriorWars/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Warrior goodGuy = new Warrior("Bob", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Joe", Faction.BadGuy);
+
+            goodGuy.Attack(badGuy)
+
+        }
+    }
+}
---
+++ WarriorWars/Equipment/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars.Equipment
+{
+    class Weapon
+    {
+        private const int GOOD_GUY_DAMAGE = 5;
+        private const int BAD_GUY_DAMAGE = 5;
+
+        private int damage;
+
+        public int Damage
+        {
+            get
+            {
+                return Damage;
+            }
+        }
+
+        public Weapon(Faction faction)
+        {
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    damage = GOOD_GUY_DAMAGE;
+                    break;
+                case Faction.BadGuy:
+                    damage = BAD_GUY_DAMAGE;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
